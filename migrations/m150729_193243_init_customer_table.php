39ced7879bb928834ab9977524f9ae2be283ff26
Kirill Marenov <kmarenov@gmail.com>
2015-07-29 22:42:22 +0300

    init customer table migration

---
+++ migrations/m150729_193243_init_customer_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+class m150729_193243_init_customer_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'customer',
+            [
+                'id' => 'pk',
+                'name' => 'string',
+                'birth_date' => 'date',
+                'notes' => 'text',
+            ],
+            'ENGINE=InnoDB'
+        );
+    }
+
+    public function down()
+    {
+        $this->dropTable('customer');
+    }
+}
