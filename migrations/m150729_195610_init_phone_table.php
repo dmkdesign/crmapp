5113f97022e30ba4d9e424b24e383b8da3f71ee5
Kirill Marenov <kmarenov@gmail.com>
2015-07-29 23:04:05 +0300

    init phone table migration

---
+++ migrations/m150729_195610_init_phone_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m150729_195610_init_phone_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'phone',
+            [
+                'id' => 'pk',
+                'customer_id' => 'int unique',
+                'number' => 'string',
+            ],
+            'ENGINE=InnoDB'
+        );
+
+        $this->addForeignKey('customer_phone_numbers', 'phone', 'customer_id', 'customer', 'id');
+    }
+
+    public function down()
+    {
+        $this->dropForeignKey('customer_phone_numbers', 'phone');
+
+        $this->dropTable('phone');
+    }
+}
