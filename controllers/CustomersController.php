1e6f6415f8c94182f1372e17fc85e089a4dc9687
Kirill Marenov <kmarenov@gmail.com>
2015-08-03 22:02:24 +0300

    CustomerController::store() method

--- controllers/CustomersController.php
+++ controllers/CustomersController.php
@@ -1,6 +1,9 @@
 <?php
 namespace app\controllers;
 
+use app\models\customer\Customer;
+use app\models\customer\CustomerRecord;
+use app\models\customer\PhoneRecord;
 use yii\web\Controller;
 
 class CustomersController extends Controller
@@ -9,5 +12,22 @@ class CustomersController extends Controller
     {
         $records = $this->findRecordsByQuery();
         return $this->render('index', compact('records'));
+    }
+
+    private function store(Customer $customer)
+    {
+        $customer_record = new CustomerRecord();
+        $customer_record->name = $customer->name;
+        $customer_record->birth_date = $customer->birth_date->format('Y-m-d');
+        $customer_record->notes = $customer->notes;
+
+        $customer_record->save();
+
+        foreach ($customer->phones as $phone) {
+            $phone_record = new PhoneRecord();
+            $phone_record->number = $phone->number;
+            $phone_record->customer_id = $customer_record->id;
+            $phone_record->save();
+        }
     }
 }
