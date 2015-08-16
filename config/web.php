6018b43d0fb64b30a84fa490994af0d8e7e21a98
Kirill Marenov <kmarenov@gmail.com>
2015-08-16 21:50:29 +0300

    enable pretty urls

--- config/web.php
+++ config/web.php
@@ -7,5 +7,9 @@ return [
             'cookieValidationKey' => '1021c636f4ae69fb3eb71c66441c9458',
         ],
         'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false
+        ]
     ]
 ];
