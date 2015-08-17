d4cadf6340931f67f3f58a0808b0c433dc2151ff
Kirill Marenov <kmarenov@gmail.com>
2015-08-17 19:43:50 +0300

    customer controller load method

--- controllers/CustomersController.php
+++ controllers/CustomersController.php
@@ -19,7 +19,21 @@ class CustomersController extends Controller
     {
         $customer = new CustomerRecord();
         $phone = new PhoneRecord();
+
+        if ($this->load($customer, $phone, $_POST)) {
+            $this->store($this->makeCustomer($customer, $phone));
+            return $this->redirect('/customers');
+        }
+
         return $this->render('add', compact('customer', 'phone'));
+    }
+
+    private function load(CustomerRecord $customer, PhoneRecord $phone, array $post)
+    {
+        return $customer->load($post)
+        and $phone->load($post)
+        and $customer->validate()
+        and $phone->validate(['number']);
     }
 
     private function store(Customer $customer)
