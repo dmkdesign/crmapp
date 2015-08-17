e45f679b07456ac91f07f97d7c630f65216b0ebf
Kirill Marenov <kmarenov@gmail.com>
2015-08-17 19:45:01 +0300

    customers controller add view typo fix

--- views/customers/add.php
+++ views/customers/add.php
@@ -20,7 +20,6 @@ $form = ActiveForm::begin([
 echo $form->errorSummary([$customer, $phone]);
 echo $form->field($customer, 'name');
 echo $form->field($customer, 'birth_date');
-echo $form->field($customer, 'birth_date');
 echo $form->field($customer, 'notes');
 
 echo $form->field($phone, 'number');
