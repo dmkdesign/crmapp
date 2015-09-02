e52e649f0f9f3c11e4bfbd396d5774a836454342
Kirill Marenov <kmarenov@gmail.com>
2015-09-02 22:24:04 +0300

    CustomersController::getRecordsByPhoneNumber()

--- controllers/CustomersController.php
+++ controllers/CustomersController.php
@@ -24,6 +24,35 @@ class CustomersController extends Controller
         return $dataProvider;
     }
 
+    private function getRecordsByPhoneNumber($number)
+    {
+        $phone_record = PhoneRecord::findOne(['number' => $number]);
+        if (!$phone_record) {
+            return [];
+        }
+
+        $customer_record = CustomerRecord::findOne($phone_record->customer_id);
+        if (!$customer_record) {
+            return [];
+        }
+
+        return [$this->makeCustomer($customer_record, $phone_record)];
+    }
+
+    private function makeCustomer(
+        CustomerRecord $customer_record,
+        PhoneRecord $phone_record
+    ) {
+        $name = $customer_record->name;
+        $birth_date = new \DateTime($customer_record->birth_date);
+
+        $customer = new Customer($name, $birth_date);
+        $customer->notes = $customer_record->notes;
+        $customer->phones[] = new Phone($phone_record->number);
+
+        return $customer;
+    }
+
     private function wrapIntoDataProvider($data)
     {
         return new ArrayDataProvider(
@@ -70,19 +99,5 @@ class CustomersController extends Controller
             $phone_record->customer_id = $customer_record->id;
             $phone_record->save();
         }
-    }
-
-    private function makeCustomer(
-        CustomerRecord $customer_record,
-        PhoneRecord $phone_record
-    ) {
-        $name = $customer_record->name;
-        $birth_date = new \DateTime($customer_record->birth_date);
-
-        $customer = new Customer($name, $birth_date);
-        $customer->notes = $customer_record->notes;
-        $customer->phones[] = new Phone($phone_record->number);
-
-        return $customer;
     }
 }
