911d5340003e571082c62fa339dbf40c5a51856d
dmkdesign.ca <aaron@dmkdesign.ca>
2019-11-11 23:32:34 -0800

    Update console.php

--- config/console.php
+++ config/console.php
@@ -5,6 +5,19 @@ return [
     'components' => [
         'db' => require(__DIR__ . '/db.php'),
     ],
+    'modules' => [
+        'attachments' => [
+            'class' => nemmo\attachments\Module::className(),
+            'tempPath' => '@app/uploads/temp',
+            'storePath' => '@app/uploads/store',
+            'rules' => [ // Rules according to the FileValidator
+                'maxFiles' => 1, // Allow to upload maximum 3 files, default to 3
+                'mimeTypes' => 'image/png', // Only png images
+                'maxSize' => 1024 * 1024 // 1 MB
+            ],
+            'tableName' => '{{%attachments}}' // Optional, default to 'attach_file'
+        ]
+    ],
     'controllerMap' => [
         'migrate' => [
             'class' => 'yii\console\controllers\MigrateController',
