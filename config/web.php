40dc04357c47671144a07e52bb2da2974870e8ba
dmkdesign.ca <aaron@dmkdesign.ca>
2019-11-11 21:28:16 -0800

    Update web.php

--- config/web.php
+++ config/web.php
@@ -11,5 +11,26 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false
         ]
-    ]
+        ],
+    'modules' => [
+            'attachments' => [
+            'class' => nemmo\attachments\Module::className(),
+            'tempPath' => '@app/uploads/temp',
+            'storePath' => '@app/uploads/store',
+            'rules' => [ // Rules according to the FileValidator
+                'maxFiles' => 1, // Allow to upload maximum 3 files, default to 3
+                'mimeTypes' => 'image/png', // Only png images
+                'maxSize' => 1024 * 1024 // 1 MB
+            ],
+            'tableName' => '{{%attachments}}' // Optional, default to 'attach_file'
+        ]
+    ],
+    'controllerMap' => [
+        'migrate' => [
+            'class' => 'yii\console\controllers\MigrateController',
+            'migrationNamespaces' => [
+                'nemmo\attachments\migrations',
+            ],
+        ],
+        ],
 ];
